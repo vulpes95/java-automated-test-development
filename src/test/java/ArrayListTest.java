df6cd46df77a5f8b5f08f8a29ecb828b5b733a77
eugene.krivosheyev@gmail.com <eugene.krivosheyev@gmail.com>
2020-05-12 12:10:13 +0300

    Add auto-test scenario layout.

---
+++ src/test/java/ArrayListTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test Case
+ */
+public class ArrayListTest { //SUT
+    @Test
+    public void shouldGetElementWhenAddNullElement() { //BDD
+        //region Fixture | Arrange | Given
+        ArrayList<Object> sut = new ArrayList<>();
+        Object dummyElement = null;
+        //endregion
+
+        //region Act | When
+        sut.add(dummyElement);
+        //endregion
+
+        //region Assert | Then
+        assertEquals("", dummyElement, sut.get(0));
+        //endregion
+    }
+}
