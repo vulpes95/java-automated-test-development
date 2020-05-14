4236528e2a39882b29bf30b1c1479461e096927d
eugene.krivosheyev@gmail.com <eugene.krivosheyev@gmail.com>
2020-05-14 11:41:25 +0300

    Improve unit test with fixture reuse and assumptions.

--- src/test/java/demo/ArrayListTest.java
+++ src/test/java/demo/ArrayListTest.java
@@ -1,20 +1,37 @@
 package demo;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
 
 /**
  * Test Case
  */
 public class ArrayListTest { //SUT
+    private ArrayList<Object> sut;
+
+    public ArrayListTest() {
+        System.out.println(">>>> CREATED!!!!!");
+    }
+
+    //    @BeforeClass, static!!!
+    @Before //@After
+    public void initSut() {
+        sut = new ArrayList<>();
+    }
+
     @Test
     public void shouldGetElementWhenAddNullElement() { //BDD
         //region Fixture | Arrange | Given
-        ArrayList<Object> sut = new ArrayList<>();
         Object dummyElement = null;
+        assumeTrue(sut.size() == 0);
         //endregion
 
         //region Act | When
@@ -23,6 +40,12 @@ public class ArrayListTest { //SUT
 
         //region Assert | Then
         assertEquals("", dummyElement, sut.get(0));
+        assertEquals(1, sut.size());
         //endregion
+    }
+
+    @Test
+    public void shouldBeingEmptyWhenCreated() {
+        assertEquals(0, sut.size());
     }
 }
